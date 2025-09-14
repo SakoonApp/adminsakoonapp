21e79284a3f3359117724f4c937ffdea2abaaecd
rahul <tom334957@gmail.com>
2025-09-14 13:36:15 +0530

    Replace Repo1 code with Repo2 files

--- functions/src/utils/admin.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from "firebase-admin";
-
-// Initialize the app once for the entire functions runtime
-admin.initializeApp();
-
-// Export initialized services for use in other functions
-export const db = admin.firestore();
-export const auth = admin.auth();
-export const storage = admin.storage();
